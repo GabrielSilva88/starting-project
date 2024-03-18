ecf33f178bdc2f3eda91454f8ab92554451ff791
GabrielSilva88 <gabrielmg.silva@outlook.com>
2024-03-18 15:59:14 -0300

    Seção 10: Projeto Carrinho de Compras 118. Começando o desafio.Atualizado

---
+++ src/pages/Home/index.js
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { View, Text, StyleSheet, SafeAreaView, FlatList, TouchableOpacity } from "react-native";
+
+import { Feather } from "@expo/vector-icons";
+  
+export default function Home() {
+    const [products, setProducts] = useState([
+        { id: '1', name: "Coca cola", price: 19.90 },
+        { id: '2', name: "Chocolate", price: 6.50 },
+        { id: '3', name: "Queijo 500g", price: 15 },
+        { id: '4', name: "Batata Frita", price: 23.90 },
+        { id: '5', name: "Guarana lata", price: 6.00 },
+        { id: '6', name: "Atum em lata", price: 14.75 }
+    ])
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.cartContent}>
+                <Text style={styles.title}> Lista de Produtos </Text>
+                <TouchableOpacity style={styles.cartButton}>
+                    <View style={styles.dot}>
+                        <Text style={styles.dotText}>3</Text>
+                    </View>
+                    <Feather name="shopping-cart" size={30} color={"#000"}/>
+                </TouchableOpacity>
+            </View>
+            
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingStart: 14,
+        paddingEnd: 14,
+    },
+    cartContent:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center',
+        marginTop:24,
+        marginBottom:24
+    },
+    title:{
+        fontSize:24,
+        fontWeight:'bold',
+    },
+    dot:{
+        alignItems:'center',
+        justifyContent: 'center',
+        backgroundColor:'red',
+        width:20,
+        height:20,
+        borderRadius: 12,
+        position:'absolute',
+        zIndex:99,
+        bottom:-2,
+        left:-4
+    },
+    dotText:{
+        fontSize:12
+    }
+})
